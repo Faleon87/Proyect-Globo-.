e4afe55ab0b5651c26b6f511f07aafa2d1be5396
Jaime <jaimevijuescafraca@gmail.com>
2023-12-06 14:31:12 +0100

    .
    .

--- android-main/LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/descripcion_restaurante/view/DescripcionRestaurantView.java
+++ android-main/LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/descripcion_restaurante/view/DescripcionRestaurantView.java
@@ -1,6 +1,9 @@
 package com.example.loginandroid_29_09_2023.descripcion_restaurante.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,12 +11,15 @@ import com.example.loginandroid_29_09_2023.R;
 import com.example.loginandroid_29_09_2023.beans.User;
 import com.example.loginandroid_29_09_2023.descripcion_restaurante.ContractDescripcionRest;
 import com.example.loginandroid_29_09_2023.descripcion_restaurante.presenter.DescripcionRestaurantPresenter;
+import com.example.loginandroid_29_09_2023.restaurant_puntuacion.view.RestaurantOrderRatingView;
 
 public class DescripcionRestaurantView extends AppCompatActivity implements ContractDescripcionRest.View{
 
     private DescripcionRestaurantPresenter presenter =
             new DescripcionRestaurantPresenter(this);
     private static DescripcionRestaurantView mainActivity = null;
+
+    private ImageButton volver3;
 
     public static DescripcionRestaurantView getInstance(){
         return mainActivity;
@@ -28,7 +34,14 @@ public class DescripcionRestaurantView extends AppCompatActivity implements Cont
     }
 
     private void initComponents(){
-
+            volver3 = findViewById(R.id.volver3);
+            volver3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(DescripcionRestaurantView.this, RestaurantOrderRatingView.class);
+                    startActivity(intent);
+                }
+            });
     }
 
 
